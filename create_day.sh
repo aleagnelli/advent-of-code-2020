c86ec52785b63c82a100763a7c92f5c8679396f2
Alessandro Agnelli <agnelli.jedi@gmail.com>
2020-12-07 15:44:04 +0100

    Add script to generate new day template

---
+++ create_day.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+day=$(printf "%02d" "${1}")
+#touch "lib/advent_of_code/day_${day}.ex"
+cat << EOF > "lib/advent_of_code/day_${day}.ex"
+defmodule AdventOfCode.Day${day} do
+  @doc """
+      iex> AdventOfCode.Day${day}.part1()
+      1
+  """
+  def part1(input) do
+    0
+  end
+
+  @doc """
+      iex> AdventOfCode.Day${day}.part2()
+      1
+  """
+  def part2(input) do
+    0
+  end
+
+end
+
+EOF
+touch "priv/day_${day}.txt"
+cat << EOF > "test/advent_of_code/day_${day}_test.exs"
+defmodule Day${day}Test do
+  use ExUnit.Case
+  doctest AdventOfCode.Day${day}
+
+  test "part 1" do
+    input = get_resource()
+    res = AdventOfCode.Day${day}.part1(input)
+    assert res == 1
+  end
+
+  test "part 2" do
+    input = get_resource()
+    res = AdventOfCode.Day${day}.part2(input)
+    assert res == 1
+  end
+
+  def get_resource() do
+    "priv/day_${day}.txt" |> File.read!()
+  end
+end
+
+EOF
